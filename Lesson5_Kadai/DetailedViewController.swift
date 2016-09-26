8c29729e92c4327932423416ffda742d0e8e4e2e
Toru Yoshihara <viterbi0713@gmail.com>
2016-09-26 14:31:53 +0900

    ooo

---
+++ Lesson5_Kadai/DetailedViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailedViewController.swift
+//  Lesson5_Kadai
+//
+//  Created by Toru Yoshihara on 2016/09/26.
+//  Copyright © 2016年 tooru.yoshihara. All rights reserved.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
